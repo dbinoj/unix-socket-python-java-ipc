62fbe167c3be2e110a4620549d75fa725ffc9ddb
Binoj David <dbinoj@crowdanalytix.com>
2017-03-22 18:40:25 +0530

    add java prog

--- hello_model.py
+++ hello_model.py
@@ -1,26 +1,29 @@
 import os
 import socketserver
 import socket
-# import time
+import time
 
 sock_file = "/tmp/hello_model.sock"
 
 
 class HelloModel(socketserver.BaseRequestHandler):
     def handle(self):
+        t1 = time.time()
         self.data = self.request.recv(5242880).strip()
         data_str = bytes.decode(self.data)
         # for i in range(5):
         #     time.sleep(1)
         #     print(i)
-        resp_str = "Hello, {}".format(data_str)
+        resp_str = "Hello, {}!".format(data_str)
         self.request.sendall(str.encode(resp_str))
+        t2 = time.time()
+        print("{}s".format(t2-t1))
 
 
 class ThreadingUnixStreamServer(
     socketserver.ThreadingMixIn, socketserver.UnixStreamServer
 ):
-    socket_type = socket.SOCK_SEQPACKET
+    socket_type = socket.SOCK_STREAM
 
 if __name__ == "__main__":
     if os.path.exists(sock_file):
---
+++ java/Test.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import com.etsy.net.*;
+
+public class Test {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		if (args.length != 1) {
+			System.out
+					.println("usage: java TestUnixDomainSocket socketfilename");
+			System.exit(1);
+		}
+		String socketFile = args[0];
+		long socStartTime = System.nanoTime();
+
+		UnixDomainSocketClient socket = new UnixDomainSocketClient(socketFile,
+				JUDS.SOCK_STREAM);
+		long socEndTime = System.nanoTime();
+		InputStream in = socket.getInputStream();
+		OutputStream out = socket.getOutputStream();
+		long startTime = System.nanoTime();
+		String text = "Java";
+		out.write(text.getBytes());
+		String resp = "";
+		for (int b = 0; ((b = in.read()) >= 0);) {
+            resp += (char) b;
+        }
+        long endTime = System.nanoTime();
+
+		socket.close();
+		System.out.println(resp);
+		System.out.println("Nanoseconds to open socket                 : " + Long.toString(socEndTime - socStartTime));
+		System.out.println("Nanoseconds to send, process & receive data: " + Long.toString(endTime - startTime));
+
+	}
+}
